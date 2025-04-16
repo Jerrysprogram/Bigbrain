1c00984f4d7de122950de418aade0e2989567df7
z5484763 <z5484763@ad.unsw.edu.au>
2025-04-17 00:46:01 +1000

    third part

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -115,3 +115,74 @@ const App = () => {
         console.log('Validate Failed:', errInfo);
       }
     });
+    const columns = [
+      {
+        title: 'name',
+        dataIndex: 'name',
+        width: '25%',
+        editable: true,
+      },
+      {
+        title: 'age',
+        dataIndex: 'age',
+        width: '15%',
+        editable: true,
+      },
+      {
+        title: 'address',
+        dataIndex: 'address',
+        width: '40%',
+        editable: true,
+      },
+      {
+        title: 'operation',
+        dataIndex: 'operation',
+        render: (_, record) => {
+          const editable = isEditing(record);
+          return editable ? (
+            <span>
+              <Typography.Link onClick={() => save(record.key)} style={{ marginInlineEnd: 8 }}>
+                Save
+              </Typography.Link>
+              <Popconfirm title="Sure to cancel?" onConfirm={cancel}>
+                <a>Cancel</a>
+              </Popconfirm>
+            </span>
+          ) : (
+            <Typography.Link disabled={editingKey !== ''} onClick={() => edit(record)}>
+              Edit
+            </Typography.Link>
+          );
+        },
+      },
+    ];
+    const mergedColumns = columns.map(col => {
+      if (!col.editable) {
+        return col;
+      }
+      return Object.assign(Object.assign({}, col), {
+        onCell: record => ({
+          record,
+          inputType: col.dataIndex === 'age' ? 'number' : 'text',
+          dataIndex: col.dataIndex,
+          title: col.title,
+          editing: isEditing(record),
+        }),
+      });
+    });
+    return (
+      <Form form={form} component={false}>
+        <Table
+          components={{
+            body: { cell: EditableCell },
+          }}
+          bordered
+          dataSource={data}
+          columns={mergedColumns}
+          rowClassName="editable-row"
+          pagination={{ onChange: cancel }}
+        />
+      </Form>
+    );
+  };
+  export default App;
