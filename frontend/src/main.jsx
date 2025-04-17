540b8e4cff0b79ea8302a9ada1432f6a6006521d
z5484763 <z5484763@ad.unsw.edu.au>
2025-04-17 20:38:03 +1000

    add react router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,31 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import 'antd/dist/reset.css'
+
 import App from './App.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/login',
+    element: <div>Login page</div>
+  },
+  {
+    path: '/register',
+    element: <div>Register page</div>
+  },
+  {
+    path: '/dashboard',
+    element: <div>Dashboard page</div>
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
