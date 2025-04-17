290b168d114113de76efc97024a043fce7dbd341
z5484763 <z5484763@ad.unsw.edu.au>
2025-04-18 01:34:45 +1000

    update the dashboard

--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -1,7 +1,33 @@
+import React from 'react';
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import { Avatar, Card } from 'antd';
+import { Link } from 'react-router-dom';
+const { Meta } = Card;
+
 export default function Dashboard() {
   return (
     <div>
       <h1>Dashboard</h1>
+      <Card
+        style={{ width: 300 }}
+        cover={
+          <img
+            alt="example"
+            src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+          />
+        }
+        actions={[
+          <SettingOutlined key="setting" />,
+          <EditOutlined key="edit" />,
+          <EllipsisOutlined key="ellipsis" />,
+        ]}
+      >
+        <Meta
+          avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />}
+          title={<Link to="/games">Games</Link>}
+          description="This is the description"
+        />
+      </Card>
     </div>
   )
 }
