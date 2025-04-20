e8c02b9b958f5894fb77f9aba5f7b0fc99109c3c
z5484763 <z5484763@ad.unsw.edu.au>
2025-04-20 22:13:57 +1000

    add put function

--- frontend/src/utills/requests.js
+++ frontend/src/utills/requests.js
@@ -13,7 +13,7 @@ const defaultOptions = {
 function get (url) {
 }
 
-//**
+/**
 * 封装 post 函数
 * @param {*} url 
 * @param {*} data 传输的数据，对象
@@ -44,3 +44,36 @@ function post(url, data) {
      .catch(err => message.error(err.message))
  );
 }
+
+/**
+ * 封装 put 函数
+ * @param {*} url
+ * @param {*} data 传输的数据，对象
+ * @returns
+ */
+function put (url, data) {
+    return (
+      fetch(`${BASE_HOST}${url}`, {
+        ...defaultOptions,
+        method: 'PUT',
+        headers: {
+          ...defaultOptions.headers,
+          // 从 localStorage 中获取 token
+          Authorization: `Bearer ${window.localStorage.getItem('token')}`,
+        },
+        body: JSON.stringify(data),
+      })
+        // 解析 json
+        .then(res => res.json())
+        // 返回解析过的数据
+        .then(data => {
+          if (data.error) {
+            throw new Error(data.error);
+          }
+          return data;
+        })
+        // 捕获到错误，显示错误消息
+        .catch(err => message.error(err.message))
+    );
+  }
+  
