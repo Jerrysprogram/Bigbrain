d075316930b757b99b61c38abb70f0b1c6e62d21
z5484763 <z5484763@ad.unsw.edu.au>
2025-04-20 22:10:39 +1000

    add post function

--- frontend/src/utills/requests.js
+++ frontend/src/utills/requests.js
@@ -14,17 +14,33 @@ function get (url) {
 }
 
 //**
- * 封装 post 函数
- * @param {*} url
- * @param {*} data 传输的数据, 对象
- * @returns
- */
-function post (url, data) {
-  return (
-    fetch(`${BASE_HOST}${url}`, {
-      ...defaultOptions,
-      method: 'POST',
-      headers: {
-        ...defaultOptions.headers,
-        // 从 localStorage 中获取 token
-        Authorization: `Bearer ${window.localStorage.getItem('token')}`,
+* 封装 post 函数
+* @param {*} url 
+* @param {*} data 传输的数据，对象
+* @returns 
+*/
+function post(url, data) {
+ return (
+   fetch(`${BASE_HOST}${url}`, {
+     ...defaultOptions,
+     method: 'POST',
+     headers: {
+       ...defaultOptions.headers,
+       // 从 localStorage 中获取 token
+       Authorization: `Bearer ${window.localStorage.getItem('token')}`,
+     },
+     body: JSON.stringify(data),
+   })
+     // 解析 json
+     .then(res => res.json())
+     // 返回解析过的数据
+     .then(data => {
+       if (data.error) {
+         throw new Error(data.error);
+       }
+       return data;
+     })
+     // 捕获到错误，显示错误消息
+     .catch(err => message.error(err.message))
+ );
+}
