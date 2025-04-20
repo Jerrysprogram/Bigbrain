d768315405c6075fbc0c2917791e74eda5363915
z5484763 <z5484763@ad.unsw.edu.au>
2025-04-20 22:24:31 +1000

    add fetch

--- frontend/src/utills/requests.js
+++ frontend/src/utills/requests.js
@@ -10,8 +10,30 @@ const defaultOptions = {
 };
 
 // 封装 fetch
-function get (url) {
-}
+function get(url) {
+    // Return Promise
+    return (
+      fetch(`${BASE_HOST}${url}`, {
+        ...defaultOptions,
+        headers: {
+          ...defaultOptions.headers,
+          // 从 localStorage 中获取 token
+          Authorization: `Bearer ${window.localStorage.getItem('token')}`,
+        },
+      })
+        // 解析 json
+        .then(res => res.json())
+        // 返回解析过的数据
+        .then(data => {
+          if (data.error) {
+            throw new Error(data.error);
+          }
+          return data;
+        })
+        // 捕获到错误，显示错误消息
+        .catch(err => message.error(err.message))
+    );
+  }  
 
 /**
 * 封装 post 函数
@@ -76,4 +98,11 @@ function put (url, data) {
         .catch(err => message.error(err.message))
     );
   }
-  
+
+const requests = {
+  get,
+  post,
+  put,
+};
+
+export default requests;
