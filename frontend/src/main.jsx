fb8dfd917b6b344067ce13573d0bf17eaf007638
z5484763 <z5484763@ad.unsw.edu.au>
2025-04-24 23:05:45 +1000

    change

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,68 +1,71 @@
 // import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider, createBrowserRouter, Navigate } from 'react-router-dom'
-import 'antd/dist/reset.css'
-import { Spin } from 'antd'
+import { createRoot } from "react-dom/client";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  Navigate,
+} from "react-router-dom";
+import "antd/dist/reset.css";
 
-import Login from './pages/login/login.jsx'
-import Register from './pages/register/register.jsx'
-import Dashboard from './pages/dashboard/dashboard.jsx'
-import Games from './pages/gamepages/games.jsx'
-import GameEdit from './pages/gamepages/GameEdit.jsx'
-import QuestionEdit from './pages/gamepages/QuestionEdit.jsx'
-import SessionAdmin from './pages/gamepages/SessionAdmin.jsx'
-import Join from './pages/play/Join.jsx'
-import Play from './pages/play/Play.jsx'
+import Login from "./pages/login/login.jsx";
+import Register from "./pages/register/register.jsx";
+import Dashboard from "./pages/dashboard/dashboard.jsx";
+import Games from "./pages/gamepages/games.jsx";
+import GameEdit from "./pages/gamepages/GameEdit.jsx";
+import QuestionEdit from "./pages/gamepages/QuestionEdit.jsx";
+import SessionAdmin from "./pages/gamepages/SessionAdmin.jsx";
+import Join from "./pages/play/Join.jsx";
+import Play from "./pages/play/Play.jsx";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Navigate to="/dashboard" />,
   },
   {
-    path: '/login',
+    path: "/login",
     element: <Login />,
   },
   {
-    path: '/register',
+    path: "/register",
     element: <Register />,
   },
   {
-    path: '/dashboard',
+    path: "/dashboard",
     element: <Dashboard />,
   },
   {
-    path: '/play/join',
-    element: <Join />
+    path: "/play/join",
+    element: <Join />,
   },
   {
-    path: '/play/join/:sessionId',
-    element: <Join />
+    path: "/play/join/:sessionId",
+    element: <Join />,
   },
   {
-    path: '/play/:playerId',
+    path: "/play/:playerId",
     element: <Play />,
   },
   {
-    path: '/game/:gameId',
+    path: "/game/:gameId",
     element: <GameEdit />,
   },
   {
-    path: '/game/:gameId/question/:questionId',
+    path: "/game/:gameId/question/:questionId",
     element: <QuestionEdit />,
   },
   {
-    path: '/games/:gameId',
+    path: "/games/:gameId",
     element: <Games />,
   },
   {
-    path: '/session/:sessionId',
-    element: <SessionAdmin />
-  }
+    path: "/session/:sessionId",
+    element: <SessionAdmin />,
+  },
 ]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   // <StrictMode>
-    <RouterProvider router={router} />
+  <RouterProvider router={router} />,
   // </StrictMode>,
-)
+);
