b5438fb78ec0175dadd098cbea4219a36df9710f
z5484763 <z5484763@ad.unsw.edu.au>
2025-04-24 23:34:48 +1000

    change

--- frontend/src/pages/dashboard/dashboard.jsx
+++ frontend/src/pages/dashboard/dashboard.jsx
@@ -4,7 +4,6 @@ import { Link, useNavigate } from 'react-router-dom';
 import { UserOutlined, PlusOutlined, EditOutlined, DeleteOutlined, CopyOutlined } from '@ant-design/icons';
 import { ifLogin } from '../../utills/index';
 import requests from '../../utills/requests';
-import { getGameById } from '../../utills/api';
 
 export default function Dashboard() {
   const [modalVisible, setModalVisible] = useState(false);
